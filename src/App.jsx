45c9fd963e8889204ff586c09db05a20d981c738
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-10-27 15:32:36 +0100

    Updated websocket path

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import GameArea from './Compenents/GameArea'
 import Hand from './Compenents/Hand'
 import { useState } from 'react'
 
-const ws = new WebSocket("ws://localhost:12345/echo");
+const ws = new WebSocket("ws://localhost:12345/ws");
 
 
 
 
 
-function MakeBet() { 
+function MakeBet() {
   console.log("Hi from Jim")
 }
 
@@ -18,9 +18,9 @@ function sendMove(playerMove) {
   let GameID = 1234
   let PlayerID = 123
   console.log(`Sending ${playerMove}`)
-  let str_move = JSON.stringify({GameID, PlayerID, MoveSTR: playerMove})
+  let str_move = JSON.stringify({ GameID, PlayerID, MoveSTR: playerMove })
   ws.send(str_move)
-  
+
 }
 
 
@@ -32,7 +32,7 @@ function App() {
 
   function betAttempted() {
     // const betButton = event.target
-    
+
 
     console.log(`Bet Attempted: ${betRankBox.value} ${selectedIndex}`)
     if (Number(betRankBox.value) < 1) {
@@ -40,12 +40,12 @@ function App() {
     }
     // other validation here
 
-    
+
     //Passed all validation
     let playerMove = betRankBox.value + " " + selectedIndex
     sendMove(playerMove)
   }
-  
+
 
   console.log(`The current value of selectedIndex is ${selectedIndex}`);
   return (
@@ -65,7 +65,7 @@ function App() {
             <div className="bet_multiplier"><input type="number" name="rank" />x</div>
             <Hand values={["one", "two", "three", "four", "five", "six"]} selectable={true} setSelectedIndex={setSelectedIndex}></Hand>
           </div>
-          <button className="Make_Bet" type="button" onClick ={() => betAttempted()}>Make Bet</button>
+          <button className="Make_Bet" type="button" onClick={() => betAttempted()}>Make Bet</button>
           <button className="Dudo" type="button" onClick={() => sendMove("Dudo")}>Dudo</button>
           <button className="Calza" type="button" onClick={() => sendMove("Calza")}>Calza</button>
         </div>
