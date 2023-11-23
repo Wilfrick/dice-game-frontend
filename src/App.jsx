fff92c5154419c5763896e4e89446e139526e459
BroadbentJim <jim.r.broadbent@gmail.com>
2023-11-23 11:26:49 +0000

    Styled game. Currently not showing the previous hand

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import validRelativePreviousBet from './Utils/validRelativePreviousBet'
 import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import LandingPage from './Components/LandingPage'
 import LobbyPage from './Components/LobbyPage'
+import GameDisplay from './Components/GameDisplay'
 
 function App() {
 
@@ -50,13 +51,18 @@ function App() {
   registerWsCallback(wsStateDictionary)
 
 
-  allCurrentHands[playerClientIndex] = currentPlayerHand
+  // allCurrentHands[playerClientIndex] = currentPlayerHand
 
-  let isMyTurn = (playerClientIndex == currentTurn)
-  let canMakeBet = betRankBoxValue && selectedIndex && (validRelativePreviousBet(betRankBoxValue, selectedIndex, movesMade[0]?.MoveMade.Value))
-  let canDudo = movesMade[0]?.MoveMade.MoveType == "Bet"
-  let canCalza = (allCurrentHands.filter(x => x.length).length > 2) && canDudo
-  let lastBetRankMade = movesMade[1]?.MoveMade.Value.FaceVal
+  // let isMyTurn = (playerClientIndex == currentTurn)
+  // let canMakeBet = betRankBoxValue && selectedIndex && (validRelativePreviousBet(betRankBoxValue, selectedIndex, movesMade[0]?.MoveMade.Value))
+  // let canDudo = movesMade[0]?.MoveMade.MoveType == "Bet"
+  // let canCalza = (allCurrentHands.filter(x => x.length).length > 2) && canDudo
+  // let lastBetRankMade = movesMade[1]?.MoveMade.Value.FaceVal
+
+  let gameStateDictionary = {
+    currentPlayerHand, currentHoveredValue, playerClientIndex, currentTurn, allCurrentHands, selectedIndex, setSelectedIndex, 
+    betRankBoxValue, setBetRankBoxValue, setCurrentHoveredValue, movesMade, roundRevealHands,
+  }
 
   // console.log(`The current value of selectedIndex is ${selectedIndex}`);
   return (
@@ -64,55 +70,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
       <Route path="/lobby/" element={<LobbyPage numPlayers={lobbyPlayerCount} navigate={navigate} LobbyID={lobbyID}/>}></Route>
-      <Route path="/game/" element={<>
-        <GameArea>
-
-          <div className={`hands${currentHoveredValue ? " " + currentHoveredValue : ""} selected`}>
-            <p>You are player: {playerClientIndex}. It is currently Player {currentTurn}'s turn</p>
-            {/* <Hand values={["one", "two", "three", "three", "four"]}></Hand>
-          <Hand values={currentPlayerHand}></Hand> */}
-            {allCurrentHands.map(
-              (hand, i) => <LabelledHand values={hand} playerName={i} key={i}></LabelledHand>
-            )}
-          </div>
-          <div className={`hands${lastBetRankMade ? " " + numberToString(lastBetRankMade) : ""} selected`}> {/* surly there is a better way than a ? : */}
-            <p>Starting old hands</p>
-            {roundRevealHands.map(
-              (hand, j) => <LabelledHand values={hand} playerName={j} key={j + 20}></LabelledHand>
-            )}
-          </div>
-          <div className="action_display">
-            <div className="my_actions">
-              <BetSelector selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}
-                betRankBoxValue={betRankBoxValue} setBetRankBoxValue={setBetRankBoxValue} setCurrentHoveredValue={setCurrentHoveredValue} />
-              <button className="Make_Bet" type="button" onClick={() => betAttempted(betRankBoxValue, selectedIndex)} disabled={!(isMyTurn && canMakeBet)}>Make Bet</button>
-              <button className="Dudo" type="button" onClick={() => sendMove("Dudo")} disabled={!(isMyTurn && canDudo)}>Dudo</button>
-              <button className="Calza" type="button" onClick={() => sendMove("Calza")} disabled={!(isMyTurn && canCalza)}>Calza</button>
-            </div>
-            <div className="bet_history">
-              {movesMade.map(
-                (move, i) => <PlayerAction MoveMade={move.MoveMade} PlayerIndex={move.PlayerIndex} key={i} />
-              )}
-              <Bet player_identifier="Jim" bet_multiplier={3} value="two" />
-              <Bet player_identifier="Alexander the first" bet_multiplier={3} value="three" />
-
-
-              <Dudo />
-              <div className="action dudo">
-                <p className="player_identifier">Player: 5</p>
-                Dudo!</div>
-
-              <div className="action calza">
-                <p className="player_identifier">Player: 1</p>
-                Calza!</div>
-
-            </div>
-          </div>
-        </GameArea >
-        <div className='my_actions'>
-          <button className="start_game" type="button" onClick={() => { sendGameStart(); console.log("StartGameButton") }}>Start Game</button>
-        </div>
-      </>}>
+      <Route path="/game/" element={<GameDisplay gameStateDictionary={gameStateDictionary}/>}>
       </Route>
     </Routes>
   )
---
+++ src/Components/GameDisplay.jsx
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types'
+import LabelledHand from './LabelledHand';
+import BetSelector from './BetSelector';
+import GameArea from './GameArea';
+import { betAttempted } from '../Services/Connection';
+import { sendMove } from '../Services/Connection';
+import validRelativePreviousBet from '../Utils/validRelativePreviousBet';
+import PlayerAction from './PlayerAction';
+import { numberToString, stringToNumber } from '../Utils/numberParser';
+const GameDisplay = ({gameStateDictionary : {currentPlayerHand, currentHoveredValue, playerClientIndex, currentTurn, allCurrentHands, selectedIndex, setSelectedIndex, 
+    betRankBoxValue, setBetRankBoxValue, setCurrentHoveredValue, movesMade, roundRevealHands}}) =>
+{
+    
+    allCurrentHands[playerClientIndex] = currentPlayerHand
+
+    let isMyTurn = (playerClientIndex == currentTurn)
+    let canMakeBet = betRankBoxValue && selectedIndex && (validRelativePreviousBet(betRankBoxValue, selectedIndex, movesMade[0]?.MoveMade.Value))
+    let canDudo = movesMade[0]?.MoveMade.MoveType == "Bet"
+    let canCalza = (allCurrentHands.filter(x => x.length).length > 2) && canDudo
+    let lastBetRankMade = movesMade[1]?.MoveMade.Value.FaceVal
+
+    return (
+        <GameArea>
+            <div className="game_info">
+            <h4> It is currently <span className='player_name'>Player {currentTurn}'s </span> turn</h4>
+            
+            <div className="hand_display">
+                <div className={`hands${currentHoveredValue ? " " + currentHoveredValue : ""} selected`}>
+           
+            
+                {allCurrentHands.map(
+                    (hand, i) => <LabelledHand values={hand} key={i} isCurrentPlayer={currentTurn==i}></LabelledHand>
+                )}
+                </div>
+                    <div className="player_names">{allCurrentHands.map((_, i) => <p className={`${currentTurn == i ? "current_player" : ""}`}> Player < span className="player_name">{i}</span> </p>)}
+                </div>
+            </div>
+            <h4>You are player: {playerClientIndex}</h4>
+            </div>
+          {/* <div className={`hands${lastBetRankMade ? " " + numberToString(lastBetRankMade) : ""} selected`}> 
+            <p>Starting old hands</p>
+            {roundRevealHands.map(
+              (hand, j) => <LabelledHand values={hand} playerName={j} key={j + 20}></LabelledHand>
+            )}
+          </div> */}
+          <div className="action_display">
+            <div className="my_actions">
+              <BetSelector selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}
+                            betRankBoxValue={betRankBoxValue} setBetRankBoxValue={setBetRankBoxValue} setCurrentHoveredValue={setCurrentHoveredValue} isMyTurn={isMyTurn}/>
+              <button className={`Make_Bet ${isMyTurn ? " my_turn": ""}`} type="button" onClick={() => betAttempted(betRankBoxValue, selectedIndex)} disabled={!(isMyTurn && canMakeBet)}>Make Bet</button>
+                        <button className={`Dudo ${isMyTurn ? " my_turn": ""}`} type="button" onClick={() => sendMove("Dudo")} disabled={!(isMyTurn && canDudo)}>Dudo</button>
+                        <button className={`Calza ${isMyTurn ? " my_turn": ""}`} type="button" onClick={() => sendMove("Calza")} disabled={!(isMyTurn && canCalza)}>Calza</button>
+            </div>
+            <div className="bet_history">
+              {movesMade.map(
+                (move, i) => <PlayerAction MoveMade={move.MoveMade} PlayerIndex={move.PlayerIndex} key={i} />
+              )}
+              {/* <Bet player_identifier="Jim" bet_multiplier={3} value="two" />
+              <Bet player_identifier="Alexander the first" bet_multiplier={3} value="three" />
+
+
+              <Dudo />
+              <div className="action dudo">
+                <p className="player_identifier">Player: 5</p>
+                Dudo!</div>
+
+              <div className="action calza">
+                <p className="player_identifier">Player: 1</p>
+                Calza!</div> */}
+
+            </div>
+          </div>
+        </GameArea >
+
+  )
+}
+
+export default GameDisplay
+
